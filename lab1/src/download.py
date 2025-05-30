8d04a963499441a922c1d3c70d8c1eaf432162e4
Maria <Maria>
2025-05-30 21:30:59 +0300

    sales to gitignore

--- lab1/src/download.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("omercolakoglu/10million-rows-turkish-market-sales-dataset")
-
-print("Path to dataset files:", path)
